e08c83c4a99ff5082750d3ab1c5177a0c6e0a8c3
Sanjay Kushwaha <dev.sanjaykushwaha@gmail.com>
2025-08-16 07:38:56 +0000

    const firebaseConfig = {   apiKey: "AIzaSyD7-zoVT79X82jDwzEW0N5IBlfV0-fq

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,20 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyD7-zoVT79X82jDwzEW0N5IBlfV0-fqHgo",
+  authDomain: "mandiexpress-40url.firebaseapp.com",
+  projectId: "mandiexpress-40url",
+  storageBucket: "mandiexpress-40url.firebasestorage.app",
+  messagingSenderId: "74214106113",
+  appId: "1:74214106113:web:6f72472662b4e284618c00"
 };
 
 // Initialize Firebase
 let app;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
 }
 
 const auth = getAuth(app);
